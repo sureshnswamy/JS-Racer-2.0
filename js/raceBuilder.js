8d4f8eda9fac83d5ede87f0a6e9a5edf627cc153
sureshnswamy <sureshnswamy@gmail.com>
2016-02-09 18:32:24 +1300

    js set canavas variables

--- js/raceBuilder.js
+++ js/raceBuilder.js
@@ -1,86 +1,24 @@
 
-/*
-
-This file contains all of the code running in the background that makes raceBuilder.js
-*/
-
-
-$(document).ready(function() {
-// choose the track length
-
-
-var player1Move = 0;
-
-var player2Move = 0;
-
-var rowLength = prompt('Please choose your track length between 10 - 50');
-console.log(rowLength);
-
-if (rowLength<10) {
-    var rowLength = 10;
-    alert("You have choosen a number out of range so the track length set to 10. Press OK to start play")
-  } else if (rowLength >50) {
-     var rowLength = 10;
-      alert("You have choosen a number out of range so the track length set to 10. Press OK to start play")
-  };
-
-
-var gameStart = function () {
-$("td").removeClass("active");
-$("#player1 td:first").addClass("active");
-
-$("#player2 td:first").addClass("active");
-
-var player1Move = 0;
-var player2Move = 0;
-
-};
-
-for (var i=0; i<= rowLength; i++) {
-        $(".row").append('<td>');
-        };
-
-gameStart();
-
-//keypress event 
-
-$(document).on('keyup',function(keyPress) {
-
-    if (keyPress.keyCode === 80){ 
-
-        if (player1Move < rowLength -1) {
-        updatePlayerPosition("player1");
-        player1Move++;
-        }  else  {
-            raceWinner("Player1");
-        };
-
-    } else if (keyPress.keyCode === 81) { 
-
-        if (player2Move < rowLength -1) {
-        updatePlayerPosition("player2");
-        player2Move++;
-        }  else  {
-            raceWinner("Player2");
-        };
+// Canvas Variables
+    var canvas = $('#canvas');
+    var context = canvas.get(0).getContext('2d');
+   
+    var start_time = new Date().getTime();
     
-   };
- });
- //update active td
-    var updatePlayerPosition = function (player) {
-    var activeTD = $("#" + player + " td.active");
-    var moveTD = activeTD.next();
-
-    activeTD.removeClass("active");
-    moveTD.addClass("active");
-  };
+    var background = new Image();
+    background.src = "images/track2.png";
 
   
-  var raceWinner = function (winner) {
-    window.alert (winner + " wins the race YaY! Press OK to play again.")
-    gameStart();
 
-  };
+    // set timer for the game
+    function getTimer () {
+    return (new Date().getTime() - start_time); //milliseconds
+    }
 
-});
-
+    // Keyboard Variables
+    var leftKey = 37;
+    var upKey = 38;
+    var rightKey = 39;
+    var downKey = 40;
+    
+   
